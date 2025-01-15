46652cb6d3cb08a1db88ac663a3f1252b7bcf13a
Amir <baradaran13085@gmail.com>
2025-01-16 01:49:43 +0330

    Update Index.js

---
+++ Js/Index.js
@@ -0,0 +1,44 @@
+'use strict'
+// Start Slider //
+
+const imgsSlidEl=document.querySelectorAll('.sliderImg')
+const prevSlidEl=document.querySelector('.prev-slider')
+const nextSlidEl=document.querySelector('.next-slider')
+let activeSlider=0
+
+const nextSlidFunc=()=>{
+    imgsSlidEl[activeSlider].classList.remove('show')
+    activeSlider++
+    if(activeSlider===imgsSlidEl.length-1)activeSlider=0
+    imgsSlidEl[activeSlider].classList.add('show')
+}
+
+setInterval(nextSlidFunc,5000)
+
+nextSlidEl.addEventListener('click',nextSlidFunc)
+
+prevSlidEl.addEventListener('click',()=>{
+    imgsSlidEl[activeSlider].classList.remove('show')
+    activeSlider--
+    if(activeSlider===-1)activeSlider=imgsSlidEl.length-1
+    imgsSlidEl[activeSlider].classList.add('show')    
+})
+
+// Finish Slider 
+
+// start modal offer //
+// const modal=document.querySelector('.modal')
+
+// window.onload=()=>{
+//     modal.classList.add('active')
+// }
+// setTimeout(()=>{
+// modal.classList.remove('active')
+// },5000)
+// window.onclick=(e)=>{
+//     if(e.target!==modal){
+//         modal.classList.remove('active')
+//     }
+// }
+
+// finish modal offer //
